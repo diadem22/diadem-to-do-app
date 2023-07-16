aa8ea7ba78d2e8ac83dc964ce969f8dfcde12cf2
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-07-16 22:11:53 +0100

    fix

--- app.js
+++ app.js
@@ -23,38 +23,37 @@ app.use(express.json());
     const { name, password, role } = req.body;
 
     
-    bcrypt.hash(password, 10).then(async (hash) => {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: 'Password less than 6 characters' });
+    }
+    try {
+      bcrypt.hash(password, 10).then(async (hash) => {
     await createUser({
       name,
       password: hash,
-      role
     })
-      .then((user) => {
-        const maxAge = 3 * 60 * 60;
-        const token = jwt.sign(
-          { name, role },
-          process.env.SECRET_TOKEN,
-          {
-            expiresIn: maxAge, 
-          }
-        );
-        res.cookie('jwt', token, {
-          httpOnly: true,
-          maxAge: maxAge * 1000, 
-        });
-        res.status(201).json({
-          message: 'User successfully created',
-          user
-        });
-      })
+      .then((user) =>
+        res.status(200).json({
+          message: "User successfully created",
+           user,
+        })
+      )
       .catch((error) =>
         res.status(400).json({
-          message: 'User not successful created',
+          message: "User not successful created",
           error: error.message,
         })
       );
   })
-  });
+  }catch (error) {
+    res.status(400).json({
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
+})
 
   app.get('/to-do/login', async (req, res, next) => {
     const { name, password } = req.body;
