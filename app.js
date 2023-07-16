a43fc0497ee46f57dea3e6696b5cb26383be4fff
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-07-16 21:32:16 +0100

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt')
 const app = express();
 const {
   createActivity,
