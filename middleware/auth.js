7c4a7793bf5efc3a96ef7abc6cd8014d07661057
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-07-19 22:36:21 +0100

    formatting

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,61 +5,34 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+async function verifyToken(req, res, next) {
+  const authHeader = req.headers['cookie'];
 
-async function verifyToken (req, res, next) {
-    const authHeader = req.headers['cookie'];
+  if (!authHeader) return res.sendStatus(401);
+  const cookie = authHeader.split('=')[1];
 
-    if (!authHeader) return res.sendStatus(401);
-    const cookie = authHeader.split('=')[1];
+  const checkBlacklist = await Blacklist.findOne({ token: cookie });
+  if (checkBlacklist)
+    return res
+      .status(401)
+      .json({ message: 'Session expired. Please re-login' });
 
-    const checkBlacklist = await Blacklist.findOne({ token: cookie });
-    if (checkBlacklist)
-        return res
-            .status(401)
-            .json({ message: 'Session expired. Please re-login' });
+  try {
+    jwt.verify(cookie, process.env.SECRET_TOKEN, async (err, decoded) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
 
-//     jwt.verify(cookie, process.env.SECRET_TOKEN,  async (err, decoded) => {
-//         if (err) {
-//             return res.sendStatus(403);
-//         }
-
-//     const { id } = decoded; 
-//     const user = await User; 
-//     const { password, ...data } = user._doc; 
-//     req.user = data; 
-//     next();
-//   });
-
-      try {
-    jwt.verify(cookie, process.env.SECRET_TOKEN,  async (err, decoded) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
-
-    const { id } = decoded; 
-    const user = await User; 
-    const { password, ...data } = user._doc; 
-    req.user = data; 
-    next();
-    })
+      const { id } = decoded;
+      const user = await User;
+      const { password, ...data } = user._doc;
+      req.user = data;
+      next();
+    });
   } catch (err) {
     return res.status(401).send('Invalid Token');
   }
   return next();
-};
+}
 
-
-
-//   if (!token) {
-//     return res.status(403).send('No token provided');
-//   }
-//   try {
-//     const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
-//     req.user = decoded;
-//   } catch (err) {
-//     return res.status(401).send('Invalid Token');
-//   }
-//   return next();
-// };
-
-module.exports = {verifyToken};
+module.exports = { verifyToken };
--- routes/activity.js
+++ routes/activity.js
@@ -11,62 +11,54 @@ const { verifyToken } = require('../middleware/auth');
 
 const router = express.Router();
 
+router.post('/create', verifyToken, async (req, res, next) => {
+  const { user_id, name, category, date, isPublished, priority } = req.body;
 
-    router.post(
-      '/create',
-      verifyToken,
-      async (req, res, next) => {
-        const { user_id, name, category, date, isPublished, priority } =
-          req.body;
-
-        try {
-          const activity = await createActivity(
-            user_id,
-            name,
-            category,
-            date,
-            isPublished,
-            priority
-          );
-          return res
-            .status(200)
-            .json({
-              data: activity,
-              success: true,
-              message: 'Activity created',
-            });
-        } catch (error) {
-          return next(error);
-        }
-      }
+  try {
+    const activity = await createActivity(
+      user_id,
+      name,
+      category,
+      date,
+      isPublished,
+      priority
     );
-
-    router.put('/update', verifyToken, async (req, res, next) => {
-      const { id, name, priority, category } = req.body;
-
-      try {
-        const activity = await updateActivity(id, name, category, priority);
-        return res
-          .status(200)
-          .json({ data: activity, success: true, message: 'Activity updated' });
-      } catch (error) {
-        return next(error);
-      }
+    return res.status(200).json({
+      data: activity,
+      success: true,
+      message: 'Activity created',
     });
+  } catch (error) {
+    return next(error);
+  }
+});
 
-    router.get('/fetch', verifyToken, async (req, res, next) => {
-      const { user_id } = req.body;
+router.put('/update', verifyToken, async (req, res, next) => {
+  const { id, name, priority, category } = req.body;
 
-      try {
-        const activity = await fetchById(user_id);
-        return res.status(200).json({
-          data: activity,
-          success: true,
-          message: 'Activity retrieved',
-        });
-      } catch (error) {
-        return next(error);
-      }
+  try {
+    const activity = await updateActivity(id, name, category, priority);
+    return res
+      .status(200)
+      .json({ data: activity, success: true, message: 'Activity updated' });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get('/fetch', verifyToken, async (req, res, next) => {
+  const { user_id } = req.body;
+
+  try {
+    const activity = await fetchById(user_id);
+    return res.status(200).json({
+      data: activity,
+      success: true,
+      message: 'Activity retrieved',
     });
+  } catch (error) {
+    return next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
