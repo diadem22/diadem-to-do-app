47da014d0f6353cfadbacd4e6ae7b0cbba6001a8
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-07-20 15:06:11 +0100

    display user_id after login

--- routes/user.js
+++ routes/user.js
@@ -60,6 +60,7 @@ router.post('/login', async (req, res, next) => {
         res.status(200).json({
         status: 'success',
         message: 'You have successfully logged in.',
+        user_id: user._id
         } );
         } 
     }catch (error) {
