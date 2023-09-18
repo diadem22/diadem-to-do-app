2290b54ed4ff2720dff905cc3190b36d222161dc
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2023-09-18 23:29:40 +0100

    added k8s folder

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 dockek build -t diadem27/to-do-app:latest -f ./app/Dockerfile ./app
 docker push diadem27/to-do-app:latest
+kubectl apply -f app/k8s
+kubectl set image deployment.apps/web-deployment server=diadem27/to-do-app:latest
